927a78997542e11be91a136b24874c13cc73e73d
Ghanshyam Rai <gsrai082002@gmail.com>
2023-10-12 11:15:50 +0530

    Completed task is working correctly

--- task.js
+++ task.js
@@ -1,7 +1,10 @@
 let tasks = JSON.parse(localStorage.getItem('taskInfo')) || [];
 
+let completedTasks = JSON.parse(localStorage.getItem('completedInfo')) || [];
+
 const taskForm = document.querySelector('.form');
 const taskList = document.querySelector('.task-list');
+const completedTaskList = document.querySelector('.completed-task-list');
 
 taskForm.addEventListener('submit',(e) =>{
     e.preventDefault();
@@ -57,8 +60,41 @@ function displayTasks(){
         const checkbox = document.getElementById(`task-completed-${index}`);
         checkbox.addEventListener('change', () =>{
             tasks[index].completed = checkbox.checked;
+
+            if(tasks[index].completed){
+                completedTasks.push(tasks[index]);
+                tasks.splice(index, 1);
+                localStorage.setItem('taskInfo', JSON.stringify(tasks));
+                displayTasks();
+                displayCompletedTasks();
+            }
         });
     });
+}
+
+function displayCompletedTasks(){
+    completedTaskList.innerHTML = '';
+
+    completedTasks.forEach((task,index) => {
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('completed-task-item');
+
+        taskItem.innerHTML = `
+            <div class="completed-info">
+                <h3>${task.title}</h3>
+                <p>Due Date: ${task.dueDate}</p>
+            </div>
+            <p>${task.description}</p>
+
+            <div class="completed-delete">
+                <button onclick="deleteCompletedTask(${index})">Delete</button>
+            </div>
+        `;
+
+        completedTaskList.appendChild(taskItem);
+    });
+
+    localStorage.setItem('completedInfo', JSON.stringify(completedTasks));
 }
 
 function editTask(index){
@@ -82,7 +118,14 @@ function deleteTask(index){
     displayTasks();
 }
 
+function deleteCompletedTask(index){
+    completedTasks.splice(index,1);
+    localStorage.setItem('completedInfo', JSON.stringify(completedTasks));
+    displayCompletedTasks();
+}
+
 displayTasks();
+displayCompletedTasks();
 
 /*
 About e.preventDefault();
